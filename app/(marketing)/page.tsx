dd08d50132ad3b60e15c796962646a15913207f8
Giovanni Gonzaga <giovannigonzaga@Giovannis-MBP.attlocal.net>
2024-03-19 11:36:26 -0700

    build marketing skeleton

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="lg" variant="destructive">Click me!</Button>
+      <div>Marketing page</div>
     </div>
   )
 }
